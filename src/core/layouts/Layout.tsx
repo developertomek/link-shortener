0bd1682bf4d33c6e7f1b6746059f7cbcc8211d6c
developertomek <wisniewskit94@gmail.com>
2023-10-27 14:15:42 +0200

    change - layout and home page

--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -1,16 +1,49 @@
 import Head from "next/head"
-import React, {FC} from "react"
-import {BlitzLayout} from "@blitzjs/next"
+import React, { FC } from "react"
+import { BlitzLayout } from "@blitzjs/next"
+import { AppShell, Center, Container, Footer, Group, Header, Text } from "@mantine/core"
 
-const Layout: BlitzLayout<{title?: string; children?: React.ReactNode}> = ({title, children}) => {
+const Layout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
+  title,
+  children,
+}) => {
+  const appName = "Shortl"
+  const thisYear = new Date().getFullYear()
+
   return (
     <>
       <Head>
         <title>{title || "link-shortener"}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      {children}
+      <AppShell
+        mx="auto"
+        padding="md"
+        header={
+          <Header height={60} px="xs" withBorder={false}>
+            <Container>
+              <Group mx="auto" h={60} align="center">
+                <Text fw={700}>{appName}</Text>
+              </Group>
+            </Container>
+          </Header>
+        }
+        footer={
+          <Footer withBorder={false} height={50} p="md" fz="xs" color="dimmed">
+            <Center>
+              {appName} @copyright {thisYear}
+            </Center>
+          </Footer>
+        }
+        styles={(theme) => ({
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
+          },
+        })}
+      >
+        <Container size="md">{children}</Container>
+      </AppShell>
     </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { Suspense } from "react"
 import Link from "next/link"
 import Layout from "src/core/layouts/Layout"
 import { useCurrentUser } from "src/users/hooks/useCurrentUser"
 import logout from "src/auth/mutations/logout"
 import { useMutation } from "@blitzjs/rpc"
 import { Routes, BlitzPage } from "@blitzjs/next"
-import { Button } from "@mantine/core"
+import { Button, Group, List, Stack, ThemeIcon, Title, Text, rem, Image } from "@mantine/core"
+import { IconCheck } from "@tabler/icons-react"
 
 const UserInfo = () => {
   const currentUser = useCurrentUser()
@@ -37,6 +37,52 @@ const UserInfo = () => {
         <Link href={Routes.LoginPage()}>
           <strong>Login</strong>
         </Link>
+
+        <Group>
+          <Stack>
+            <Title>
+              A <span>modern</span> React <br /> components library
+            </Title>
+            <Text c="dimmed" mt="md">
+              Build fully functional accessible web applications faster than ever – Mantine includes
+              more than 120 customizable components and hooks to cover you in any situation
+            </Text>
+
+            <List
+              mt={30}
+              spacing="sm"
+              size="sm"
+              icon={
+                <ThemeIcon size={20} radius="xl">
+                  <IconCheck style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
+                </ThemeIcon>
+              }
+            >
+              <List.Item>
+                <b>TypeScript based</b> – build type safe applications, all components and hooks
+                export types
+              </List.Item>
+              <List.Item>
+                <b>Free and open source</b> – all packages have MIT license, you can use Mantine in
+                any project
+              </List.Item>
+              <List.Item>
+                <b>No annoying focus ring</b> – focus ring will appear only when user navigates with
+                keyboard
+              </List.Item>
+            </List>
+
+            <Group mt={30}>
+              <Button radius="xl" size="md">
+                Get started
+              </Button>
+              <Button variant="default" radius="xl" size="md">
+                Source code
+              </Button>
+            </Group>
+          </Stack>
+          <Image src="logo.png" alt="" maw={100} />
+        </Group>
       </>
     )
   }
