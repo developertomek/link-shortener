331d58fc9fcc8bef9ec6b4b526425011a0182a99
developertomek <wisniewskit94@gmail.com>
2023-10-29 18:45:53 +0100

    add mutation to save generated links

---
+++ src/links/mutations/generateLink.ts
@@ -0,0 +1,26 @@
+import { resolver } from "@blitzjs/rpc"
+import { z } from "zod"
+import db from "db"
+
+const Input = z.object({
+  url: z.string(),
+  redirectTo: z.string().url(),
+})
+
+export default resolver.pipe(
+  resolver.zod(Input),
+  resolver.authorize(),
+  async ({ url, redirectTo }, { session: { userId } }) => {
+    return await db.link.create({
+      data: {
+        name: url,
+        redirectTo,
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    })
+  }
+)
