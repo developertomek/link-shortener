a25f3f95f3dd0f91c092df561210aeb237b48aaf
developertomek <wisniewskit94@gmail.com>
2023-10-30 09:18:11 +0100

    change margin and disabled when loading

--- src/core/components/LinkInput.tsx
+++ src/core/components/LinkInput.tsx
@@ -5,11 +5,11 @@ import { Button, Text, TextInput } from "@mantine/core"
 export const LinkInput: ReactFC<{
   onChange?: (e: React.FormEvent<HTMLInputElement>) => void
   onClick?: () => void
-  isLoading: boolean
-  redirectTo?: string
-}> = ({ onChange, onClick, redirectTo, isLoading = false }) => (
+  isLoading?: boolean
+}> = ({ onChange, onClick, isLoading = false }) => (
   <TextInput
     radius="md"
+    mt="sm"
     size="lg"
     maw={600}
     w="100%"
@@ -22,7 +22,7 @@ export const LinkInput: ReactFC<{
         loading={isLoading}
         onClick={onClick}
         size="sm"
-        disabled={isLoading || !redirectTo}
+        disabled={isLoading}
         rightIcon={<IconArrowRight stroke={1.5} width={12} />}
       >
         <Text size="sm">Generate</Text>
