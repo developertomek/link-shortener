fe19d5df5f5eef1671ec9ac8ba3a5dbd638a304c
developertomek <wisniewskit94@gmail.com>
2023-10-31 12:47:16 +0100

    add value to linkilnput

--- src/core/components/LinkInput.tsx
+++ src/core/components/LinkInput.tsx
@@ -6,27 +6,31 @@ export const LinkInput: ReactFC<{
   onChange?: (e: React.FormEvent<HTMLInputElement>) => void
   onClick?: () => void
   isLoading?: boolean
-}> = ({ onChange, onClick, isLoading = false }) => (
-  <TextInput
-    radius="md"
-    mt="sm"
-    size="lg"
-    maw={600}
-    w="100%"
-    placeholder="Paste link to shorten"
-    rightSectionWidth={130}
-    icon={<IconLink />}
-    onChange={onChange}
-    rightSection={
-      <Button
-        loading={isLoading}
-        onClick={onClick}
-        size="sm"
-        disabled={isLoading}
-        rightIcon={<IconArrowRight stroke={1.5} width={12} />}
-      >
-        <Text size="sm">Generate</Text>
-      </Button>
-    }
-  />
-)
+  redirectTo?: string
+}> = ({ onChange, onClick, isLoading = false, redirectTo }) => {
+  return (
+    <TextInput
+      radius="md"
+      mt="sm"
+      size="lg"
+      maw={600}
+      w="100%"
+      placeholder="Paste link to shorten"
+      rightSectionWidth={130}
+      icon={<IconLink />}
+      onChange={onChange}
+      value={redirectTo}
+      rightSection={
+        <Button
+          loading={isLoading}
+          onClick={onClick}
+          size="sm"
+          disabled={isLoading}
+          rightIcon={<IconArrowRight stroke={1.5} width={12} />}
+        >
+          <Text size="sm">Generate</Text>
+        </Button>
+      }
+    />
+  )
+}
